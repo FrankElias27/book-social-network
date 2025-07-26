62c7a1909d5c109ecb7ac3122a1a284ec089bf92
FRANK <frankmascco@gmail.com>
2025-07-26 10:42:03 -0500

    feat(auth): add authentication controller & service

---
+++ book-network/src/main/java/com/franks/book/email/EmailService.java
@@ -0,0 +1,12 @@
+package com.franks.book.email;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+
+    private final JavaMailSender
+}
