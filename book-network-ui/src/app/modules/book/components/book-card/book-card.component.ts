f7a1a91fc5712ecdbcfbe4e19b5d374b599bc7bd
FRANK <frankmascco@gmail.com>
2025-08-03 18:57:44 -0500

    feat(rating):add rating component

--- book-network-ui/src/app/modules/book/components/book-card/book-card.component.ts
+++ book-network-ui/src/app/modules/book/components/book-card/book-card.component.ts
@@ -1,15 +1,21 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { BookResponse } from '../../../../services/models';
+import { CommonModule } from '@angular/common';
+import { RatingComponent } from '../rating/rating.component';
 
 @Component({
   selector: 'app-book-card',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    RatingComponent
+  ],
   templateUrl: './book-card.component.html',
   styleUrl: './book-card.component.scss'
 })
 export class BookCardComponent {
   private _book: BookResponse = {};
+  private _manage:boolean = false;
   private _bookCover: string | undefined;
 
   get bookCover(): string | undefined {
@@ -28,4 +34,44 @@ export class BookCardComponent {
     this._book=value;
   }
 
+  get manage():boolean{
+    return this._manage;
+  }
+
+  @Input()
+  set manage(value:boolean){
+    this._manage=value;
+  }
+
+   @Output() private share: EventEmitter<BookResponse> = new EventEmitter<BookResponse>();
+  @Output() private archive: EventEmitter<BookResponse> = new EventEmitter<BookResponse>();
+  @Output() private addToWaitingList: EventEmitter<BookResponse> = new EventEmitter<BookResponse>();
+  @Output() private borrow: EventEmitter<BookResponse> = new EventEmitter<BookResponse>();
+  @Output() private edit: EventEmitter<BookResponse> = new EventEmitter<BookResponse>();
+  @Output() private details: EventEmitter<BookResponse> = new EventEmitter<BookResponse>();
+
+
+  onShare() {
+    this.share.emit(this._book);
+  }
+
+  onArchive() {
+    this.archive.emit(this._book);
+  }
+
+  onAddToWaitingList() {
+    this.addToWaitingList.emit(this._book);
+  }
+
+  onBorrow() {
+    this.borrow.emit(this._book);
+  }
+
+  onEdit() {
+    this.edit.emit(this._book);
+  }
+
+  onShowDetails() {
+    this.details.emit(this._book);
+  }
 }
