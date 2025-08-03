036f5eaa14dd5da2e886a03bbd5f1f5c22c76231
FRANK <frankmascco@gmail.com>
2025-08-03 08:49:35 -0500

    feat(interceptor): add interceptor

---
+++ book-network-ui/src/app/modules/book/pages/book-list/book-list.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { BookService } from '../../../../services/services/book.service';
+import { Router } from '@angular/router';
+import { findAllBooks } from '../../../../services/fn/book/find-all-books';
+import { PageResponseBookResponse } from '../../../../services/models';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-book-list',
+  standalone: true,
+  imports: [
+    CommonModule
+  ],
+  templateUrl: './book-list.component.html',
+  styleUrl: './book-list.component.scss'
+})
+export class BookListComponent implements OnInit {
+
+  bookResponse:PageResponseBookResponse ={};
+  page: number = 0;
+  size: number = 5;
+
+  constructor(
+    private bookService:BookService,
+    private router:Router
+  ){
+  }
+
+  ngOnInit(): void {
+    this.findAllBooks();
+  }
+
+  private findAllBooks(){
+    this.bookService.findAllBooks({
+      page: this.page,
+      size: this.size
+    }).subscribe({
+      next: (books) => {
+        this.bookResponse = books;
+
+      }
+    });
+  }
+
+}
